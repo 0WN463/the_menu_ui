9df0bf75c8d5a4728e2e471b9eb8f238b4850aae
0WN463 <>
2025-03-12 00:07:49 +0800

    Fetch from menu from endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery } from '@apollo/client';
 
+import { gql } from './__generated__/gql';
+
+
+const client = new ApolloClient<{}>({
+  uri: 'http://localhost:3000/graphql',
+  cache: new InMemoryCache(),
+});
+
+
+const GET_MENU = gql(`
+  query GetMenu { menus {label, identifier}}
+`);
+
+ 
+const Menu = () => {
+	  const { data } = useQuery(GET_MENU)
+
+	  return data?.menus.map(i => <div key={i.identifier}>{i.label}</div>)
+}
+
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+  <ApolloProvider client={client}>
+      <Menu />
+  </ApolloProvider>
   )
 }
 
