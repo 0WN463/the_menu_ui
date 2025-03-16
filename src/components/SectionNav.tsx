e0096d9c6d4c04b3f02059a9379319275477e73a
0WN463 <>
2025-03-17 01:47:23 +0800

    Add smooth scrolling when nav is clicked

--- src/components/SectionNav.tsx
+++ src/components/SectionNav.tsx
@@ -10,6 +10,11 @@ const SectionNav = ({
   return (
     <li
       className={`border-l-4 pl-2 leading-8 ${isCurrent ? "border-red-500" : "border-gray-200"}`}
+      onClick={(event) => {
+        event.preventDefault();
+        const anchor = document.querySelector(`#${id}`);
+        anchor?.scrollIntoView({ behavior: "smooth" });
+      }}
     >
       <a href={`#${id}`}>{label}</a>
     </li>
